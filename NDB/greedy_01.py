1eb1d6820a5524e30c4b7af9d67681e6e70244d0
Roha-Lee <rohagru@gmail.com>
2021-10-08 22:52:58 +0900

    New folder structure

---
+++ NDB/greedy_01.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split()) 
+cnt = 0
+while N:
+    remainder = int(N % K)
+    if remainder == 0:
+        N //= K
+        cnt += 1
+    else:
+        N -= remainder
+        cnt += remainder
+print(cnt - 1)
