6d51d1ba4193111e198b65962fabae0669500e3b
Roha-Lee <rohagru@gmail.com>
2021-10-14 19:07:20 +0900

    quick_sort

--- NDB/quick_sort_01.py
+++ NDB/quick_sort_01.py
@@ -1,26 +1,37 @@
-import random
-data = [1,2,3,4,5,6,7,8,9,0]
-random.shuffle(data)
-
-def quick_sort(data, start = 0, end = len(data)-1):
+def quick_sort(data, start=0, end=None):
+    if end is None: 
+        end = len(data)-1
     if start >= end:
         return 
-    pivot = start
+    p = start
     left = start + 1
     right = end
-
-    while left <= right:
-        while left <= end and data[left] <= data[pivot]:
+    while left<=right:
+        while left <= end and data[p] >= data[left]:
             left += 1
-        while right > start and data[right] >= data[pivot]:
+        while right >=0 and data[p] < data[right]:
             right -= 1
-        if left > right:
-            data[right], data[pivot] = data[pivot], data[right]
-        else:
+        if left <= right: 
             data[left], data[right] = data[right], data[left]
+        else:
+            data[right], data[p] = data[p], data[right]
+            p = right
     
-    quick_sort(data, start, right-1)
-    quick_sort(data, right+1, end)
-    
-quick_sort(data)
-print(data)
+    quick_sort(data, start, p-1)
+    quick_sort(data, p+1, end)
+
+import random
+for j in range(100):
+    leng = random.randint(1, 100)
+    data = [0] * leng
+    for i in range(leng):
+        data[i] = random.randint(1, 100)
+    original = data[:]
+    original.sort()
+    quick_sort(data)
+    if not data == original:
+        print(data)
+        print(original)
+        print("FAIL")
+    else:
+        print(f"test-{j} success")
