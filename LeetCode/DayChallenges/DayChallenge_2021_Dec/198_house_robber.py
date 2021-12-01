c0e78ad0354523bc2f8531fe0b58bb25024bcbb5
Roha-Lee <rohagru@gmail.com>
2021-12-01 09:21:04 +0900

    change Day Challenge folder structure

---
+++ LeetCode/DayChallenges/DayChallenge_2021_Dec/198_house_robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        DP = [0] * len(nums)
+        DP[0] = nums[0]
+        DP[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            DP[i] = max(DP[i-1], DP[i-2] + nums[i])
+        return DP[-1]
