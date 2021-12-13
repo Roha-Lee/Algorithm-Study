1cee87191f23cc6b532f16820d1b1aea1f07a491
Roha-Lee <rohagru@gmail.com>
2021-12-13 22:18:45 +0900

    Complete 12/13 Day Challenge

---
+++ LeetCode/DayChallenges/DayChallenge_2021_Dec/1446/consecutive_characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_val = 0
+        val = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                val += 1
+            else:
+                max_val = max(max_val, val)
+                val = 1
+        return max(max_val, val)
+                    
+            
