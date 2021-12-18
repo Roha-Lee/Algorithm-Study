d2e53d6cc50fa3158558e8dd0e3d41ff404a9181
Roha-Lee <rohagru@gmail.com>
2021-12-18 22:56:37 +0900

    midpoint

--- LeetCode/DayChallenges/DayChallenge_2021_Dec/221/Maximal_square_method2.py
+++ LeetCode/DayChallenges/DayChallenge_2021_Dec/221/Maximal_square_method2.py
@@ -1,16 +1,19 @@
 
-arr = [3,1,3,2,2]
+arr = [0, 4, 3, 5, 0]
 answer = 4 
 stack = []
 max_square = 0
+width = 0
 for num in arr:
-    width = 0 
     min_val = 100000000
+    if num == 0:
+        width = 0
     while stack and stack[-1] > num:
         val = stack.pop()
         width += 1
         min_val = min(val, min_val)
-    max_square = min(width, min_val) ** 2
+        # print(width, min_val)
+        max_square = max(max_square, min(width, min_val) ** 2)
     stack.append(num)
 print(max_square)
 
