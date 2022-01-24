fff817aff17b44c10e37cfe5912ef9b9268e33d6
Roha-Lee <rohagru@gmail.com>
2022-01-24 13:43:59 +0900

    0124 Day Challenge

---
+++ LeetCode/DayChallenges/DayChallenge_2022_Jan/520/detect_capital.py
@@ -0,0 +1,5 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return  word.lower() == word or \
+                word.upper() == word or \
+                word.capitalize() == word
