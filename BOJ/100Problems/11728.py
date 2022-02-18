df4aa8286a0d137638ce8644399d01aaa528dc68
Roha-Lee <rohagru@gmail.com>
2022-02-18 20:32:22 +0900

    100문제 풀이 #6완료

---
+++ BOJ/100Problems/11728.py
@@ -0,0 +1,30 @@
+import sys 
+input = sys.stdin.readline
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    a_list = list(map(int, input().split()))
+    b_list = list(map(int, input().split()))
+    a_idx, b_idx = 0, 0
+    result = []
+    while a_idx < len(a_list) and b_idx < len(b_list):
+        if a_list[a_idx] < b_list[b_idx]:
+            result.append(a_list[a_idx])
+            a_idx += 1
+        else:
+            result.append(b_list[b_idx])
+            b_idx += 1
+    
+    while a_idx < len(a_list):
+        result.append(a_list[a_idx])
+        a_idx += 1
+    
+    while b_idx < len(b_list):
+        result.append(b_list[b_idx])
+        b_idx += 1
+    
+    print(*result)
+    
+        
+        
+
+
