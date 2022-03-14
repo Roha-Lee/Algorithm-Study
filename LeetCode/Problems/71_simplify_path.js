0c3b7e7f28e20c96da3e07e42491941d0b9b7324
Roha-Lee <rohagru@gmail.com>
2022-03-14 23:39:23 +0900

    js coding test leetcode day challenge 0314

---
+++ LeetCode/Problems/71_simplify_path.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let result = [];
+    let filtered = path.split('/')
+    .filter(item => item !== '')
+    .forEach(item => {
+        if(item === '..'){
+            result?.pop();
+        }
+        else if(item !== '.'){
+            result.push(item);
+        }
+    })
+    
+    return result.reduce((prev, curr) => {
+        return prev + `/${curr}`;
+    }, '') || '/'
+    
+    
+};
