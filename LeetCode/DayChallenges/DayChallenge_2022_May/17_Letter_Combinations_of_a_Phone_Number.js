79337e96ef59a4829df2bbfae46face40740ba9a
Roha-Lee <rohagru@gmail.com>
2022-05-10 00:55:17 +0900

    trim 5/9 Day Challenge code

--- LeetCode/DayChallenges/DayChallenge_2022_May/17_Letter_Combinations_of_a_Phone_Number.js
+++ LeetCode/DayChallenges/DayChallenge_2022_May/17_Letter_Combinations_of_a_Phone_Number.js
@@ -1,26 +1,8 @@
 /**
  * @param {string} digits
- * @param {Map} map
- * @param {number} index
- * @param {string} temp
- * @param {string[]} results
- */
- const dfs = function(digits, map, index, temp, results) {
-    if(digits.length === index) {
-        results.push(temp);
-        return;
-    }
-    const curr = map.get(digits[index]);
-    for(let i = 0; i < curr.length; i += 1) {
-        dfs(digits, map, index + 1, temp + curr[i], results);
-    }
-};
-
-/**
- * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
+ var letterCombinations = function(digits) {
     if(digits.length === 0) return [];
     const map = new Map();
     const numbers = ['2', '3', '4', '5', '6', '7', '8', '9'];
@@ -29,6 +11,22 @@ var letterCombinations = function(digits) {
     numbers.forEach((num, index) => {
         map.set(num, letters[index]);
     });
-    dfs(digits, map, 0, '', results);
+    
+    /**
+     * @param {number} index
+     * @param {string} temp
+     */
+    const dfs = function(index, temp) {
+        if(digits.length === index) {
+            results.push(temp);
+            return;
+        }
+        const curr = map.get(digits[index]);
+        for(let i = 0; i < curr.length; i += 1) {
+            dfs(index + 1, temp + curr[i]);
+        }
+    };
+
+    dfs(0, '');
     return results;
 };
