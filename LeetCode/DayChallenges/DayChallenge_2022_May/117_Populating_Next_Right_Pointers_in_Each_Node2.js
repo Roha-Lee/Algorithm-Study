16d5135fabdaa3972a7706d1d7c2864bbc3b9e3a
Roha-Lee <rohagru@gmail.com>
2022-05-16 21:48:35 +0900

    5/13 new solution with javascript

---
+++ LeetCode/DayChallenges/DayChallenge_2022_May/117_Populating_Next_Right_Pointers_in_Each_Node2.js
@@ -0,0 +1,35 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+ var connect = function(root) {
+    if(!root) return null;
+    let prev = root;
+    while(prev) {
+        let dummy = new Node(0);
+        let curr = dummy;
+        while(prev) {
+            if(prev.left) {
+                curr.next = prev.left;
+                curr = curr.next;
+            }
+            if(prev.right) {
+                curr.next = prev.right;
+                curr = curr.next;
+            }
+            prev = prev.next;
+        }
+        prev = dummy.next;
+    }
+    return root;
+};
+
