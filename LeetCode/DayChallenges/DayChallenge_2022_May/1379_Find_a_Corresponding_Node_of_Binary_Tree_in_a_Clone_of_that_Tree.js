2660bff98e17fdd82692ef90e9f3e8a2d4b846f7
Roha-Lee <rohagru@gmail.com>
2022-05-17 18:25:22 +0900

    0517 Day Challenge

---
+++ LeetCode/DayChallenges/DayChallenge_2022_May/1379_Find_a_Corresponding_Node_of_Binary_Tree_in_a_Clone_of_that_Tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} node
+ * @param {number} target
+ * @return {TreeNode}
+ */
+ const findTargetFromTree = function(node, target) {
+    let result;
+    if(node === null) return null;
+    if(node.val === target) return node;
+    if(node.left) {
+        result ||= findTargetFromTree(node.left, target);
+    }
+    if(node.right) {
+        result ||= findTargetFromTree(node.right, target);
+    }
+    return result;
+}
+
+
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+var getTargetCopy = function(original, cloned, target) {
+    const targetVal = target.val;
+    return findTargetFromTree(cloned, targetVal);
+};
