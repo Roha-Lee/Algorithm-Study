e22835ea35ff8c96bb9210cb5b92a6c5861141f9
Roha-Lee <rohagru@gmail.com>
2022-06-05 15:31:28 +0900

    6/5 Day Challenge

---
+++ LeetCode/DayChallenges/DayChallenge_2022_June/52_NQueens_2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def DFS(self, results, candidates, n):
+        if n == len(candidates):
+            results.append(candidates[:])
+            return 
+        for new_col in range(n):
+            if self.is_available(new_col, candidates):
+                candidates.append(new_col)
+                self.DFS(results, candidates, n)
+                candidates.pop()
+
+    def is_available(self, col, candidates):
+        if col in candidates:
+            return False
+        row = len(candidates)
+        for qrow, qcol in enumerate(candidates):
+            if abs(qcol - col) == row - qrow:
+                return False
+        return True
+    
+    def decorate_result(self, results, n):
+        decorated_results = []
+        
+        for result in results:
+            decorated = []
+            for elem in result:
+                template = ['.']*(n-1)
+                template.insert(elem, 'Q')
+                decorated.append(''.join(template))
+            decorated_results.append(decorated)
+        return decorated_results
+        
+    def totalNQueens(self, n: int) -> int:
+        results = []
+        self.DFS(results, [], n)
+        return len(self.decorate_result(results, n))
