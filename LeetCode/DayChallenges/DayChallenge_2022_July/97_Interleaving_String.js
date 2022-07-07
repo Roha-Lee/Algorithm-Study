0b6e77c10f84cac69508af55bc4fc8e4f9965c5b
Roha-Lee <rohagru@gmail.com>
2022-07-07 21:40:10 +0900

    7/7 Day Challenge in JS

--- LeetCode/DayChallenges/DayChallenge_2022_July/97_Interleaving_String.js
+++ LeetCode/DayChallenges/DayChallenge_2022_July/97_Interleaving_String.js
@@ -5,19 +5,28 @@
  * @return {boolean}
  */
  var isInterleave = function(s1, s2, s3) {
-  const [m, n] = [s1.length, s2.length];
+  let [m, n] = [s1.length, s2.length];
+  let temp;
   if (m + n !== s3.length) return false;
-  const dp = new Array(Math.min(m, n) + 1).fill(false);
+  if (n > m) {
+      temp = n;
+      n = m;
+      m = temp;
+      temp = s1;
+      s1 = s2;
+      s2 = temp;
+  }
+  const dp = new Array(n + 1).fill(false);
   dp[0] = true;
-  for (let i = 1; i < Math.min(m, n) + 1; i += 1) {
+  for (let i = 1; i < n + 1; i += 1) {
       dp[i] = dp[i-1] && s2[i-1] === s3[i-1];
   }
-  for (let i = 1; i < Math.max(m, n) + 1; i += 1) {
+  for (let i = 1; i < m + 1; i += 1) {
       dp[0] = dp[0] && s1[i-1] === s3[i-1];
-      for (let j = 1; j < Math.min(m, n) + 1; j += 1) {
+      for (let j = 1; j < n + 1; j += 1) {
           dp[j] = 
-              s1[i-1] === s3[i+j-1] ? dp[j] : false ||
-              s2[j-1] === s3[i+j-1] ? dp[j-1]: false;
+              (s1[i-1] === s3[i+j-1] ? dp[j] : false) ||
+              (s2[j-1] === s3[i+j-1] ? dp[j-1]: false);
       }
   }
   return dp[dp.length-1];
