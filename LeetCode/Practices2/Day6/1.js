d7563ab8a7a2378ea9ba07173a50f520a45ea6cc
Roha-Lee <roha.lee@buzzvil.com>
2022-08-28 22:22:55 +0900

    Practice 2 - Day 6

---
+++ LeetCode/Practices2/Day6/1.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+ var invertTree = function(root) {
+    if (root) invert(root);
+    return root;
+};
+
+var invert = function(node) {
+    if (!node.left && !node.right) return;
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+    if (node.left) invert(node.left);
+    if (node.right) invert(node.right);
+}
