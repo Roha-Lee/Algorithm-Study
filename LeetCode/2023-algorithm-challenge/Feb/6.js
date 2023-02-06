6e6c008898e17c20aaf6b3e1df991dd586c5d0a2
Roha-Lee <roha.lee@buzzvil.com>
2023-02-06 12:41:27 +0900

    Feb 06

---
+++ LeetCode/2023-algorithm-challenge/Feb/6.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const result = [];
+    for (let i = 0; i < n; i +=1) {
+        result.push(nums[i]);
+        result.push(nums[n+i]);
+    }
+    return result;
+};
